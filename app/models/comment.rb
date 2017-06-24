bbcc61a6fddc78fd2062ef36493b1dafbfd43ac7
tera <kirin.da.yo.ne@gmail.com>
2017-06-24 16:22:29 +0000

    change

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
      belongs_to :board
 	 validates :board, presence: true
-	 validates :name, presence: true
-	 validates :content, presence: true, length: { in: 1..1000 }
+	 validates :name, presence: true,exclusion: { in: %w(select insert truncate -) }
+	 validates :content, presence: true, length: { in: 1..1000 },exclusion: { in: %w(select insert truncate -) }
 end
